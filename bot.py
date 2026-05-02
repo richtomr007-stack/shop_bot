932cb4e72c0e81ccdbb51d81e2d73f9273b65b05
richtomr007-stack <richtomr007@gmail.com>
2026-05-02 15:54:29 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,29 @@
 import os
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
 
-print("🚀 START")
+print("🚀 BOT STARTED")
 
-print("ENV ALL:", os.environ)
+TOKEN = os.getenv("TOKEN")
 
-print("DONE")
+print("TOKEN =", TOKEN)
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("🤖 Бот работает")
+
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(update.message.text)
+
+def main():
+    print("⚙️ BOT INIT")
+
+    app = ApplicationBuilder().token(TOKEN).build()
+
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+
+    print("✅ BOT RUNNING")
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
